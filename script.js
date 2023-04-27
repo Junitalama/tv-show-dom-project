c6696d5f317d607c82271935986f6bb3d84c349f
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-04-27 11:51:02 +0100

    level 100 done

--- script.js
+++ script.js
@@ -6,7 +6,32 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+ // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+episodeList.forEach(episode => {
+  let divEle = document.createElement("div");
+  divEle.className = "card";
+
+  const episodeName =document.createElement("h1");
+  episodeName.innerText = episode.name;
+
+  const title = document.createElement("h3");
+  title.innerText = `S0${episode.season}E0${episode.number}`;
+
+  const image = document.createElement("img");
+  image.src = episode.image["medium"];
+
+  const episodeSummary = document.createElement("p");
+  episodeSummary.innerHTML = episode.summary;
+
+  divEle.append(
+    episodeName,
+    title,
+    image,
+    episodeSummary
+);
+  rootElem.append(divEle)
+});
 }
+
 
 window.onload = setup;
